57a2e566bc49d4daf1bf0dc32fee64cac24d8f69
David Ortinau <david.ortinau@microsoft.com>
2020-02-06 09:12:57 -0600

    initial commit

---
+++ DaveTV/DaveTV/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DaveTV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new string[] { "MediaElement_Experimental" });
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DaveTV/DaveTV/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FabMDL2.ttf")]
